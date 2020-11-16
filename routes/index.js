14506d0ffbe82638fb16b9dd26b9db082e39cd20
Dain Kim <dev.dain.k@gmail.com>
2020-11-16 15:45:14 +0900

    5주 개별 페이지 READ 완료

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const template = require('../lib/template');
+
+router.get('/', (req, res) => {
+  const login = '';
+  
+  const title = "Index Page";
+  const content = 
+    "<img src='/img/pic.jpg' style='max-width: 300px; height: auto' alt='건물'>";
+  const list = template.list(req.list);
+  const create = template.create();
+  const html = template.html(title,
+    `${content}`, list, create, login);
+  res.status(200).send(html);
+});
+
+module.exports = router;
