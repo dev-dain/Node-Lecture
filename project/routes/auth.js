029854cd55717cb4dbee91e1a3d5c638dfdb844b
Dain Kim <dev.dain.k@gmail.com>
2020-11-20 15:45:28 +0900

    /auth 로직 실제 구현

---
+++ project/routes/auth.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const router = express.Router();
+const getConnection = require('../lib/db');
+const sanitizeHtml = require('sanitize-html');
+const sanitize = require('sanitize-html');
+
+router.get('/login', (req, res) => {
+  const nickname = req.session.nickname ? req.session.nickname : '';
+  const title = 'Login';
+  res.render('login', { nickname, title }, (err, html) => {
+    if (err)
+      next(err);
+    res.status(200).send(html);
+  });
+});
+
+router.post('/login', (req, res) => {
+  getConnection(conn => {
+    conn.query(`select * from user where email=? and pw=?`,
+    [sanitizeHtml(req.body.email), sanitizeHtml(req.body.pw)],
+    (err, result) => {
+      if (err || result[0] === undefined) {
+        console.error(err.stack);
+        res.redirect(302, '/auth/login');
+      } else {
+        req.session.nickname = result[0].name;
+        req.session.email = result[0].email;
+        req.session.isLogined = true;
+        res.redirect(302, '/');
+      }
+    });
+    conn.release();
+  });
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err)
+      next(err);
+    res.redirect(302, '/');
+  });
+});
+
+router.get('/join', (req, res) => {
+  const nickname = req.session.nickname ? req.session.nickname : '';
+  const title = '회원 가입 폼';
+  res.render('join', { nickname, title }, (err, html) => {
+    if (err)
+      next(err);
+    res.status(200).send(html);
+  });
+});
+
+router.post('/join', (req, res) => {
+  getConnection(conn => {
+    conn.query(`insert into user (email, pw, name, profile) values (?, ?, ?, ?)`,
+    [sanitizeHtml(req.body.email), sanitizeHtml(req.body.pw), sanitizeHtml(req.body.nickname),
+    sanitizeHtml(req.body.profile)], (err, result) => {
+      if (err)
+        next(err);
+      conn.query(`select * from user where id=?`, [result.insertId], (err2, user) => {
+        if (err2)
+          next(err2);
+        req.session.nickname = user[0].name;
+        req.session.email = user[0].email;
+        req.session.isLogined = true;
+        req.session.save(err3 => {
+          if (err3)
+            next(err3);
+          res.redirect(302, '/');
+        });
+      });
+    });
+    conn.release();
+  });
+});
+
+router.get('/update', (req, res) => {
+  getConnection(conn => {
+    conn.query(`select * from user where email=?`, [req.session.email],
+    (err, result) => {
+      if (err)
+        next(err);
+      const title = '회원 정보 수정';
+      res.render('update-user', {
+        email: result[0].email,
+        nickname: req.session.nickname,
+        profile: result[0].profile,
+        title
+      }, (err, html) => {
+        if (err)
+          next(err);
+        res.status(200).send(html);
+      });
+    });
+  });
+});
+
+router.post('/update', (req, res) => {
+  getConnection(conn => {
+    conn.query(`update user set name=?, pw=?, profile=? where email=?`,
+    [sanitizeHtml(req.body.nickname), sanitizeHtml(req.body.pw),
+    sanitizeHtml(req.body.profile), req.body.email],
+    (err, result) => {
+      if (err)
+        next(err);
+      res.status(200).send('수정이 완료되었습니다. <a href="/">홈으로 가기</a>');
+    });
+    conn.release();
+  })
+});
+
+router.get('/deactivate', (req, res) => {
+  const nickname = req.session.nickname ? req.session.nickname : '';
+  const title = '회원 탈퇴';
+  res.render('deactivate', { nickname, title },
+  (err, html) => {
+    if (err)
+      next(err);
+    res.status(200).send(html);
+  });
+});
+
+router.post('/deactivate', (req, res) => {
+  getConnection(conn => {
+    conn.query(`delete from user where email=?`, [req.session.email], (err, result) => {
+      if (err)
+        next(err);
+      req.session.destroy(err2 => {
+        if (err2)
+          next(err2);
+        res.status(200).send('안녕히 가세요. <a href="/">홈으로 가기</a>');
+      })
+    });
+    conn.release();
+  });
+});
+
+module.exports = router;
